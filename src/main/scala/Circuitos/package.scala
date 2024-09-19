098e5e03733364da5726c76d60f5168df833bfd8
juan pablo moreno hernandez <juan.moreno.hernandez@correounivalle.edu.co>
2024-09-19 00:34:55 -0500

    added adder funtion

--- src/main/scala/Circuitos/package.scala
+++ src/main/scala/Circuitos/package.scala
@@ -3,7 +3,7 @@ package object Circuitos {
   val not: Int => Int = x => 1 - x
   val or: (Int, Int) => Int = (x, y) => x + y - (x * y)
   val and: (Int, Int) => Int = (x, y) => (x * y)
-  val xor: (Int, Int) => Int = (x, y) => or(and(not(x), y), and(x, not(y)))
+  
 
   /*object Circuitos{
     def not(x:Int): Int = 1-x
@@ -22,8 +22,8 @@ package object Circuitos {
       val a = entrada.head
       val b = entrada(1)
 
-      // Suma (XOR) y Acarreo (AND) usando las funciones actuales
-      val sum = xor(a, b) // XOR usando Circuitos
+      // Suma  y Acarreo (AND) usando las funciones actuales
+      val sum = or(and(a, not(b)), and(not(a), b))
       val carry = and(a, b) // AND usando Circuitos
 
       List(carry, sum)
@@ -32,4 +32,48 @@ package object Circuitos {
 
   val ha = halfAdder
 
+  def fullAdder: Chip = {
+    entrada => {
+      val a = entrada.head
+      val b = entrada.tail.head
+      val cin = entrada.tail.tail.head
+
+      val ha1 = ha(List(a, b))
+      val ha2 = ha(List(ha1(1), cin))
+      val cout = or(ha1.head, ha2.head)
+
+      List(cout, ha2(1))
+    }
+  }
+
+  val fa = fullAdder
+
+
+  def adder(n: Int): Chip = {
+    def sumaBits(primerNumero: List[Int], segundoNumero: List[Int], carryIn: Int, acc: List[Int]): List[Int] = {
+      if (primerNumero.isEmpty) carryIn :: acc
+      else {
+        val b1 = primerNumero.head
+        val b2 = segundoNumero.head
+
+        // Suma de bits sin carry
+        val sumaI = or(and(not(b1), b2), and(b1, not(b2)))
+
+        // Nuevo carry
+        val newCarry = or(and(b1, b2), and(carryIn, or(b1, b2)))
+
+        // Suma total incluyendo el carry
+        val sumaTotal = or(and(not(sumaI), carryIn), and(sumaI, not(carryIn)))
+
+        sumaBits(primerNumero.tail, segundoNumero.tail, newCarry, sumaTotal :: acc)
+      }
+    }
+
+    input => {
+      val primerNumero = input.take(n)
+      val segundoNumero = input.drop(n)
+      sumaBits(primerNumero, segundoNumero, 0, Nil)
+    }
+  }
 }
+
--- target/scala-3.3.3/test-classes/pruebas.sc
+++ target/scala-3.3.3/test-classes/pruebas.sc
@@ -13,3 +13,25 @@ ha(List(0,0))
 ha(List(0,1))
 halfAdder(List(1,0))
 halfAdder(List(1,1))
+
+fa ( List (1 ,1 ,0))
+fa ( List (1 ,1 ,1))
+fa ( List (0 ,1 ,0))
+fa ( List (0 ,1 ,1))
+
+val add_1 = adder(1)
+add_1(List(1) ++ List(1))
+
+adder(1) (List(1) ++ List (1))
+adder(1) (List(0) ++ List (0))
+adder(1) (List(1) ++ List (0))
+adder(1) (List(0) ++ List (1))
+
+adder(2) (List(1,0) ++ List (0,1))
+adder(2) (List(1,1) ++ List (0,1))
+
+adder(3) (List(1,0,1) ++ List (0,0,0))
+adder(3) (List(1,0,1) ++ List (1,0,1))
+
+val ad4= adder(4)
+ad4(List(1,0,1,1) ++ List(1,0,1,0))
