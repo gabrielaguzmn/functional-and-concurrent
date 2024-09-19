1af2e124e6f5be3cb772db3e489d99cc94438299
juan pablo moreno hernandez <juan.moreno.hernandez@correounivalle.edu.co>
2024-09-18 19:10:09 -0500

    fixed halfAdder

--- src/main/scala/Circuitos/package.scala
+++ src/main/scala/Circuitos/package.scala
@@ -2,8 +2,8 @@ package object Circuitos {
   type Chip = List[Int] => List[Int]
   object Circuitos{
     def not(x:Int): Int = 1-x
-    def or(x:Int, y:Int ): Int = x + y - y * y
-    def and(x:Int, y:Int): Int= x * y
+    def or(x:Int, y:Int ): Int = x + y - (x * y)
+    def and(x:Int, y:Int): Int= (x * y)
     def xor(x:Int, y:Int): Int= or(and(not(x),y),and(x,not(y)))
   }
 
@@ -17,9 +17,9 @@ package object Circuitos {
       val a = entrada.head
       val b = entrada(1)
 
-      // Usa las funciones de Circuitos para calcular la suma (S) y el acarreo (C)
-      val sum = Circuitos.xor(a, b) // XOR usando la implementación de Circuitos
-      val carry = Circuitos.and(a, b) // AND usando la implementación de Circuitos
+      // Suma (XOR) y Acarreo (AND) usando las funciones actuales
+      val sum = Circuitos.xor(a, b) // XOR usando Circuitos
+      val carry = Circuitos.and(a, b) // AND usando Circuitos
 
       List(carry, sum)
     }
