48083765a1f2c3aa8b2893ff3d94add1c218bb89
juan pablo moreno hernandez <juan.moreno.hernandez@correounivalle.edu.co>
2024-09-18 19:50:20 -0500

    defined object Circuitos in other way

--- src/main/scala/Circuitos/package.scala
+++ src/main/scala/Circuitos/package.scala
@@ -1,11 +1,16 @@
 package object Circuitos {
   type Chip = List[Int] => List[Int]
-  object Circuitos{
+  val not: Int => Int = x => 1 - x
+  val or: (Int, Int) => Int = (x, y) => x + y - (x * y)
+  val and: (Int, Int) => Int = (x, y) => (x * y)
+  val xor: (Int, Int) => Int = (x, y) => or(and(not(x), y), and(x, not(y)))
+
+  /*object Circuitos{
     def not(x:Int): Int = 1-x
     def or(x:Int, y:Int ): Int = x + y - (x * y)
     def and(x:Int, y:Int): Int= (x * y)
     def xor(x:Int, y:Int): Int= or(and(not(x),y),and(x,not(y)))
-  }
+  }*/
 
   def crearChipBinario(funcion: (Int, Int) => Int): Chip = {
     entrada => List(funcion(entrada.head, entrada(1)))
@@ -18,8 +23,8 @@ package object Circuitos {
       val b = entrada(1)
 
       // Suma (XOR) y Acarreo (AND) usando las funciones actuales
-      val sum = Circuitos.xor(a, b) // XOR usando Circuitos
-      val carry = Circuitos.and(a, b) // AND usando Circuitos
+      val sum = xor(a, b) // XOR usando Circuitos
+      val carry = and(a, b) // AND usando Circuitos
 
       List(carry, sum)
     }
