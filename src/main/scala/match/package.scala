cf832662d8bfe5e9ed0adae23633fbc1faf490a3
Gabriela Guzmán <136855829+gabrielaguzmn@users.noreply.github.com>
2024-09-25 17:33:47 -0500

    add match dir with newton pkg and added pruebas-newton worksheet to test dir

---
+++ src/main/scala/match/package.scala
@@ -0,0 +1,57 @@
+package object Newton {
+
+    trait Expr
+    case class Numero ( d : Double ) extends Expr
+    case class Atomo ( x : Char ) extends Expr
+    case class Suma( e1 : Expr , e2 : Expr ) extends Expr
+    case class Prod ( e1 : Expr , e2 : Expr ) extends Expr
+    case class Resta ( e1 : Expr , e2 : Expr ) extends Expr
+    case class Div ( e1 : Expr , e2 : Expr ) extends Expr
+    case class Expo ( e1 : Expr , e2 : Expr ) extends Expr
+    case class Logaritmo ( e1 : Expr ) extends Expr
+
+    /**
+      * Ejercicio 1.1
+      * Mostrando expresiones
+      */
+
+    def mostrar(e: Expr): String {
+
+    }
+
+    /**
+      * Ejercicio 1.2
+      * Derivando expresiones
+      */
+
+    def derivar(f: Expr, a: Atomo): Expr{
+        
+    }
+  
+    /**
+      * Ejercicio 1.3
+      * Evaluando expresiones
+      */
+
+    def evaluar(f: Expr, a: Atomo, v: Double): Double{
+
+    }
+
+    /**
+      * Ejercicio 1.4
+      * Limpiando expresiones
+      */
+
+    def limpiar(f: Expr): Expr{
+
+    }
+
+    /**
+      * Ejercicio 1.5
+      * Hallando raices
+      */
+
+    def raizNewton(f: Expr, a: Atomo, x0: Double, ba:(Expr, Atomo, Double)=> Boolean): Double{
+
+    }
+}
