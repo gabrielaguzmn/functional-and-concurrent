3cc9144ee4615db4429c313caa450f45d4425944
Gabriela Guzmán <136855829+gabrielaguzmn@users.noreply.github.com>
2024-09-27 17:36:39 -0500

    add derivar, evaluar and RaizNewton methods

--- src/main/scala/match/package.scala
+++ src/main/scala/match/package.scala
@@ -1,3 +1,5 @@
+import scala.math._
+
 package object Newton {
 
     trait Expr
@@ -24,8 +26,15 @@ package object Newton {
       * Derivando expresiones
       */
 
-    def derivar(f: Expr, a: Atomo): Expr{
-        
+    def derivar(f: Expr, a: Atomo): Expr = f match{
+      case Numero(d) => Numero(0)
+      case Atomo(x) => if (Atomo(x) == a) Numero(1) else Numero(0)
+      case Suma(e1, e2) => Suma(derivar(e1, a),derivar(e2, a))
+      case Resta(e1, e2) => Resta(derivar(e1, a), derivar(e2, a))
+      case Prod(e1,e2) =>  Suma(Prod(derivar(e1,a),e2),Prod(e1,derivar(e2,a)))
+      case Div(e1,e2) => Div(Resta(Prod(derivar(e1,a),e2),Prod(e1,derivar(e2,a))),Expo(e2,Numero(2)))
+      case Expo(e1,e2) => Prod(Expo(e1,e2),Suma(Div(Prod(derivar(e1,a),e2),e1),Prod(derivar(e2, a),Logaritmo(e1))))
+      case Logaritmo(e1) => Div(derivar(e1, a), e1)
     }
   
     /**
@@ -33,8 +42,15 @@ package object Newton {
       * Evaluando expresiones
       */
 
-    def evaluar(f: Expr, a: Atomo, v: Double): Double{
-
+    def evaluar(f: Expr, a: Atomo, v: Double): Double = f match{
+        case Numero(d) => d
+        case Atomo(x) => v
+        case Suma(e1,e2) => evaluar(e1,a,v) + evaluar(e2,a,v)
+        case Resta(e1,e2) => evaluar(e1,a,v) - evaluar(e2,a,v)
+        case Prod(e1,e2) => evaluar(e1,a,v) * evaluar(e2,a,v)
+        case Div(e1,e2) => evaluar(e1,a,v) / evaluar(e2,a,v)
+        case Expo(e1,e2) => pow(evaluar(e1,a,v), evaluar(e2,a,v))
+        case Logaritmo(e1) => log(evaluar(e1,a,v))
     }
 
     /**
@@ -49,9 +65,9 @@ package object Newton {
     /**
       * Ejercicio 1.5
       * Hallando raices
-      */
-
-    def raizNewton(f: Expr, a: Atomo, x0: Double, ba:(Expr, Atomo, Double)=> Boolean): Double{
-
+      */   
+    
+    def raizNewton(f: Expr, a: Atomo, x0: Double, ba:(Expr, Atomo, Double)=> Boolean): Double = {
+        if (ba(f,a,x0)) x0 else raizNewton(f,a,x0 - evaluar(f,a,x0)/evaluar(derivar(f,a),a,x0),ba) 
     }
 }
--- src/test/scala/pruebas-newton.worksheet.sc
+++ src/test/scala/pruebas-newton.worksheet.sc
@@ -1,1 +1,23 @@
 import Newton._
+
+val expr1 = Logaritmo(Suma(Atomo('x'),Expo(Numero(2.0), Atomo('x'))))
+val expr2 = Expo(Numero(1), Prod(Atomo('x'),Logaritmo(Atomo('x'))))
+val expr3 = Resta(Atomo('x'),Div(Numero(1.0),Expo(Atomo('x'),Numero(-3.0))))
+val expr4 = Div(expr1, Logaritmo(expr2))
+val expr5 = Prod(Expo(Atomo('x'),Atomo('x')),Logaritmo(Atomo('x')))
+
+// Pruebas mostrar
+
+// Pruebas derivar
+
+// Pruebas evaluar
+
+// Pruebas limpiar
+
+// Pruebas RaizNewton
+
+def buenaAprox (f: Expr , a: Atomo , d: Double ): Boolean = {
+        evaluar(f, a, d) < 0.001
+    }
+
+
