195d55814ef2967bd21f1163ed2911c76a61cc07
Gabriela Guzmán <136855829+gabrielaguzmn@users.noreply.github.com>
2024-10-12 23:43:25 +0000

    create 'collections and for expressions' archives

---
+++ src/main/scala/collections-and-for/package.scala
@@ -0,0 +1,31 @@
+package object MatchingProblem {
+
+    type Match = (Int, Int)
+    type Matching = List[ Match ]
+    type Preferences = Vector[Vector[Int]]
+
+    def matchByElement (i: Int, n: Int) : List[Match] = {
+
+    }
+
+    def matchsByElements (n: Int) : List[List[Match]] = {
+
+    }
+
+    def possibleMatchings (n: Int) : List[List[Match]] = {
+
+    }
+
+    def matchings (n: Int) : List[Matching] = {
+
+    }
+
+    def weightedMatchings (n: Int, pilotPrefs: Preferences, navigPrefs: Preferences) : List[(Matching, Int) ] = {
+
+    }
+
+    def bestMatching (n: Int, pilotPrefs: Preferences, navigPrefs: Preferences) : (Matching, Int) = {
+        
+    }
+
+}
